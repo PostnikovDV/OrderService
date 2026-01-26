420d82cf45f4a1eb41b784dca68652f66d8d4abb
Danil <dendilpostnikov@yandex.ru>
2026-01-26 23:16:19 +0500

    убрал лишнее

--- src/Handlers/CreateOrderHandler.h
+++ src/Handlers/CreateOrderHandler.h
@@ -1,10 +1,11 @@
 #pragma once
-#include "../Utils/ServiceUtils.h"
+#include "../Orders/OrderService.h"
 
 class CreateOrderHandler
 {
 private:
     std::shared_ptr<OrderService> m_orderService;
+    OrderService::OrderInfo OrderInfoFromJson(const nlohmann::json& jsonBody);
 
 public:
     CreateOrderHandler(std::shared_ptr<OrderService> orderService)
